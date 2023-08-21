5ce7789a563ad4d23ae2f5fe428fab8c6034e37c
Petr Smáha <smazak98@seznam.cz>
2023-08-21 12:29:13 +0200

    zakladni funkce pro uziti kalkulacky

--- angular-calculator-app/src/app/calc/calc.component.ts
+++ angular-calculator-app/src/app/calc/calc.component.ts
@@ -11,14 +11,104 @@ interface Sample {
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent {
+  historyList: Sample[] = []
+  display = '0'
+  num1: number | null = null
+  operand: string | null = null
+  prevValue: string | null = null
+  binVal: string | null = null
+  hexVal: string | null = null
+  lastOper: string | null = null
 
-  historyList: Sample[] = [{
-      'equal': '1+2+3=',
-      'result': 6
-    },{
-      'equal': '1+2+3=',
-      'result': 6
+  numClick(val: number) {
+    if (this.display === '0') {
+      this.display = val.toString()
+    } else {
+      this.display = `${this.display}${val}`
     }
-  ]
+  }
 
+  oper(operand: string) {
+    if (this.prevValue && this.prevValue.length > 1 && this.prevValue.split(' ').at(-1) !== '=') {
+      this.calculate()
+    }
+
+    if (this.prevValue) {
+      this.display = `${this.num1} ${operand}`
+    }
+
+    this.num1 = parseFloat(this.display)
+    this.prevValue = `${this.num1} ${operand}`
+    this.operand = operand
+    this.display = ' '
+  }
+
+  calculate() {
+    let a = this.num1
+    let b = parseFloat(this.display)
+    let sample: Sample
+    let result: number
+    const prev = this.prevValue ? this.prevValue.split(' ') : null
+
+    if (Number.isNaN(b) && !['√','²'].includes(this.operand)) {
+      return
+    }
+
+    if (this.prevValue === null || this.display !== ' ' && prev.at(-1) === '=' ) {
+      return
+    }
+
+    switch (this.operand) {
+      case '+':
+        result = a + b
+        this.prevValue = `${a} ${this.operand} ${b} =`
+        break
+
+      case '-':
+        result = a - b
+        this.prevValue = `${a} ${this.operand} ${b} =`
+        break
+
+      case '/':
+        result = a / b
+        this.prevValue = `${a} ${this.operand} ${b} =`
+        break
+
+      case '*':
+        result = a * b
+        this.prevValue = `${a} ${this.operand} ${b} =`
+        break
+
+      case '²':
+        result = a * a
+        this.prevValue = `${a} ${this.operand} =`
+        break
+
+      case '√':
+        result = Math.sqrt(a)
+        this.prevValue = `${this.operand} ${a} =`
+        break
+    }
+
+    sample = {
+      equal: this.prevValue,
+      result: result
+    }
+
+    this.historyList.push(...[sample])
+    this.lastOper = this.operand
+    this.num1 = result
+    this.display = result.toString()
+  }
+
+  resetCalc() {
+    this.prevValue = null
+    this.display = '0'
+    this.num1 = null
+    this.operand = null
+    this.binVal = null
+    this.hexVal = null
+    this.historyList = []
+    this.lastOper = null
+  }
 }
