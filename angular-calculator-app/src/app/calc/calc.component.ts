9d28e421fe10e5757db937b24d38a01870184d2d
Petr Smáha <smazak98@seznam.cz>
2023-08-21 12:02:37 +0200

    pridani boxu pro historii prikladu

--- angular-calculator-app/src/app/calc/calc.component.ts
+++ angular-calculator-app/src/app/calc/calc.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Sample {
+  equal: string,
+  result: number
+}
+
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent {
+
+  historyList: Sample[] = [{
+      'equal': '1+2+3=',
+      'result': 6
+    },{
+      'equal': '1+2+3=',
+      'result': 6
+    }
+  ]
 
 }
